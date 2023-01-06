d08c167c325b9e53e3a3105ed76e37df85af82b5
DangTrung1212 <dangngoctrung1212@gmail.com>
2023-01-06 08:44:55 +0700

    add ls16

--- src/test/java/api_learning/FormInteracting.java
+++ src/test/java/api_learning/FormInteracting.java
@@ -1,19 +1,30 @@
 package api_learning;
 
 import driver.DriverFactory;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 
 public class FormInteracting {
     private static final WebDriver driver = DriverFactory.initDriver();
+
     public static void main(String[] args) {
         driver.get("https://the-internet.herokuapp.com/login");
-        login("#username", "#password","button[type='submit']");
+        try {
+            login("#username", "#password", "button[type='submit']");
+        } catch (Exception event) {
+            event.printStackTrace();
+        }
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         driver.navigate().refresh();
+//        need to find element again => DOM changed
+        login("#username", "#password", "button[type='submit']");
     }
+
     public static void login(String userNameSelector, String passWordSelector, String submitLocator) {
-        WebElement userNameInput= driver.findElement(By.cssSelector(userNameSelector));
+        WebElement userNameInput = driver.findElement(By.cssSelector(userNameSelector));
         WebElement passWordInput = driver.findElement(By.cssSelector(passWordSelector));
         WebElement submitButton = driver.findElement(By.cssSelector(submitLocator));
         userNameInput.sendKeys("Abc");
