6e3d420d75e001fbe37f3f3ba32a3f28554e6c57
nguyen quynh nga <nguyenquynhnga@MacBook-Air-cua-nguyen.local>
2023-01-20 15:44:22 +0700

    delete chrome driver

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -8,21 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 
 public class DriverFactory {
-    private static final String windowDriverRelativePath = "\\src\\test\\resources\\chromedriver.exe";
-    private static final String macDriverRelativePath = "/src/test/resources/chromedriver.exe";
-    private static final String rootPath = System.getProperty("user.dir");
-
     public static WebDriver initChromeDriver() {
-        String chromeDriverPath = "";
-        if (OS.isFamilyMac()) {
-            chromeDriverPath = rootPath + macDriverRelativePath;
-        } else if (OS.isFamilyWindows()) {
-            chromeDriverPath = rootPath + windowDriverRelativePath;
-
-        } else {
-            System.out.println("Not Support this OS.");
-        }
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
         ChromeOptions chromeOptions = new ChromeOptions();
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
